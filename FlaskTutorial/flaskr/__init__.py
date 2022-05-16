d556086dfe90257d99a7362658a0ec9583ca8e48
jorge <jecamg96@gmail.com>
2022-05-16 12:07:50 -0400

    __init__ correction

--- FlaskTutorial/flaskr/__init__.py
+++ FlaskTutorial/flaskr/__init__.py
@@ -32,14 +32,11 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-
-    from . import auth
-    app.register_blueprint(auth.bp)
-
-    return app
-
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
